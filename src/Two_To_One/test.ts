41a343819dbd4336785c1d9a145c11677c652dfe
habbababbai <szymanski.j.dev@gmail.com>
2026-01-16 21:35:56 +0100

    KATA: Two To One

---
+++ src/Two_To_One/test.ts
@@ -0,0 +1,16 @@
+import {longest} from './index';
+import {assert} from "chai";
+
+function testing(actual:string, expected:string) {
+    assert.strictEqual(actual, expected)
+}
+
+describe("Fixed Tests", function() {
+    it("Basic tests longest", function() {
+        testing(longest("aretheyhere", "yestheyarehere"), "aehrsty");
+        testing(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu");
+        testing(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy");
+        testing(longest("lordsofthefallen", "gamekult"), "adefghklmnorstu");
+    });
+});
+
